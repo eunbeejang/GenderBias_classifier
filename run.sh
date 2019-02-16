1db1aea98e61230c09d318b35baac05fdfdb784f
eunbeejang <eunbee.jang@mail.mcgill.ca>
2019-02-16 13:35:33 -0500

    run bash script added

---
+++ run.sh
@@ -0,0 +1,11 @@
+BATCH --time=00:10:00
+#SBATCH --job-name=sent_analysis
+#SBATCH --output=sent_analysis.out
+echo 'test started\n\n'
+python main.py
+echo '\n\ntest finished'
+~
+~
+~                                                                                                                                                             
+~                                                                                                                                                             
+~               
