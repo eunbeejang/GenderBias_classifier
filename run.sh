d691a2608e4351129facd394e92273a2dbaaf37a
Eunbee Jang <eunbee.jang@mail.mcgill.ca>
2019-02-19 12:17:54 -0800

    run script

--- run.sh
+++ run.sh
@@ -1,10 +1,24 @@
-BATCH --time=00:10:00
+#!/bin/sh
+#SBATCH --time=00:10:00
+#SBATCH --gres=gpu:1
 #SBATCH --job-name=sent_analysis
 #SBATCH --output=sent_analysis.out
 echo 'test started\n\n'
+start=$(date +%s.%N)
+
 python main.py
+
+dur=$(echo "$(date +%s.%N) - $start" | bc)
+printf "Execution time: %.6f seconds" $dur
 echo '\n\ntest finished'
 ~
+
+
+
+
+
+
+
 ~
 ~                                                                                                                                                             
 ~                                                                                                                                                             
